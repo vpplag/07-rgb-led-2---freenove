09110ca06593f2a76a37fc32e66e8902e5e2ac3c
vpplag <74141971+vpplag@users.noreply.github.com>
2021-04-11 23:42:00 +0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,3 @@
-let red = 0
-let green = 0
-let blue = 0
-let RBGcolor = 0
 function SetColor (red: number, green: number, blue: number) {
     red = Math.map(red, 0, 255, 1023, 0)
     green = Math.map(green, 0, 255, 1023, 0)
@@ -11,16 +7,17 @@ function SetColor (red: number, green: number, blue: number) {
     pins.analogWritePin(AnalogPin.P2, blue)
     basic.pause(5)
 }
+let RBGcolor = 0
+let hue = 0
+let blue = 0
+let green = 0
+let red = 0
+led.enable(false)
 basic.forever(function () {
-    for (let hue = 0; hue <= 360; hue++) {
-        RBGcolor = neopixel.hsl(hue, 100, 50)
-        red = Math.trunc(RBGcolor / 65535)
-        green = Math.trunc(RBGcolor % 65535 / 255)
-        blue = RBGcolor % 255
-        SetColor(red, green, blue)
-        led.plotBarGraph(
-        hue,
-        360
-        )
-    }
+    hue = Math.map(pins.analogReadPin(AnalogPin.P3), 0, 1023, 0, 360)
+    RBGcolor = neopixel.hsl(hue, 100, 50)
+    red = Math.trunc(RBGcolor / 65535)
+    green = Math.trunc(RBGcolor % 65535 / 255)
+    blue = RBGcolor % 255
+    SetColor(red, green, blue)
 })
